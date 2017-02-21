6dfb79c795ad38510c86d392d248fff75871ef8b
Chris Wingo <cmwingo@gmail.com>
2017-02-21 10:41:44 -0800

    initial commit, add various types of Ajax: string, sum, json & view

---
+++ src/AjaxDemo/Models/Destination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.Models
+{
+    public class Destination
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+
+        public Destination (string city, string country, int id =0)
+        {
+            City = city;
+            Country = country;
+            Id = id;
+        }
+    }
+}
